2a182217eb9347bf6460e25c73672fc8fe6006a1
mulesiddhi <mulesiddhi@gmail.com>
2021-12-30 13:12:57 +0530

    tooltips

--- public/vis.js
+++ public/vis.js
@@ -1,7 +1,12 @@
 /* global d3 */
 
+
 d3.json("10kdata.json", (error, graph) => {
-  console.log("graph", graph);
+
+  // var colorNameList=require('color-name-list');
+
+
+console.log("graph", graph);
   const nodes = graph.nodes;
   const links = graph.links;
 
@@ -15,8 +20,125 @@ d3.json("10kdata.json", (error, graph) => {
   const clusterPadding = 48; // 6
 
   const maxRadius = 12;
-  const colours = ["#6e40aa","#7140ab","#743fac","#773fad","#7a3fae","#7d3faf","#803eb0","#833eb0","#873eb1","#8a3eb2","#8d3eb2","#903db2","#943db3","#973db3","#9a3db3","#9d3db3","#a13db3","#a43db3","#a73cb3","#aa3cb2","#ae3cb2","#b13cb2","#b43cb1","#b73cb0","#ba3cb0","#be3caf","#c13dae","#c43dad","#c73dac","#ca3dab","#cd3daa","#d03ea9","#d33ea7","#d53ea6","#d83fa4","#db3fa3","#de3fa1","#e040a0","#e3409e","#e5419c","#e8429a","#ea4298","#ed4396","#ef4494","#f14592","#f34590","#f5468e","#f7478c","#f9488a","#fb4987","#fd4a85","#fe4b83","#ff4d80","#ff4e7e","#ff4f7b","#ff5079","#ff5276","#ff5374","#ff5572","#ff566f","#ff586d","#ff596a","#ff5b68","#ff5d65","#ff5e63","#ff6060","#ff625e","#ff645b","#ff6659","#ff6857","#ff6a54","#ff6c52","#ff6e50","#ff704e","#ff724c","#ff744a","#ff7648","#ff7946","#ff7b44","#ff7d42","#ff8040","#ff823e","#ff843d","#ff873b","#ff893a","#ff8c38","#ff8e37","#fe9136","#fd9334","#fb9633","#f99832","#f89b32","#f69d31","#f4a030","#f2a32f","#f0a52f","#eea82f","#ecaa2e","#eaad2e","#e8b02e","#e6b22e","#e4b52e","#e2b72f","#e0ba2f","#debc30","#dbbf30","#d9c131","#d7c432","#d5c633","#d3c934","#d1cb35","#cece36","#ccd038","#cad239","#c8d53b","#c6d73c","#c4d93e","#c2db40","#c0dd42","#bee044","#bce247","#bae449","#b8e64b","#b6e84e","#b5ea51","#b3eb53","#b1ed56","#b0ef59","#adf05a","#aaf159","#a6f159","#a2f258","#9ef258","#9af357","#96f357","#93f457","#8ff457","#8bf457","#87f557","#83f557","#80f558","#7cf658","#78f659","#74f65a","#71f65b","#6df65c","#6af75d","#66f75e","#63f75f","#5ff761","#5cf662","#59f664","#55f665","#52f667","#4ff669","#4cf56a","#49f56c","#46f46e","#43f470","#41f373","#3ef375","#3bf277","#39f279","#37f17c","#34f07e","#32ef80","#30ee83","#2eed85","#2cec88","#2aeb8a","#28ea8d","#27e98f","#25e892","#24e795","#22e597","#21e49a","#20e29d","#1fe19f","#1edfa2","#1ddea4","#1cdca7","#1bdbaa","#1bd9ac","#1ad7af","#1ad5b1","#1ad4b4","#19d2b6","#19d0b8","#19cebb","#19ccbd","#19cabf","#1ac8c1","#1ac6c4","#1ac4c6","#1bc2c8","#1bbfca","#1cbdcc","#1dbbcd","#1db9cf","#1eb6d1","#1fb4d2","#20b2d4","#21afd5","#22add7","#23abd8","#25a8d9","#26a6db","#27a4dc","#29a1dd","#2a9fdd","#2b9cde","#2d9adf","#2e98e0","#3095e0","#3293e1","#3390e1","#358ee1","#378ce1","#3889e1","#3a87e1","#3c84e1","#3d82e1","#3f80e1","#417de0","#437be0","#4479df","#4676df","#4874de","#4a72dd","#4b70dc","#4d6ddb","#4f6bda","#5169d9","#5267d7","#5465d6","#5663d5","#5761d3","#595fd1","#5a5dd0","#5c5bce","#5d59cc","#5f57ca","#6055c8","#6153c6","#6351c4","#6450c2","#654ec0","#664cbe","#674abb","#6849b9","#6a47b7","#6a46b4","#6b44b2","#6c43af","#6d41ad","#6e40aa"]
-  const z = d3.scaleOrdinal(["#6e40aa","#7140ab","#743fac","#773fad","#7a3fae","#7d3faf","#803eb0","#833eb0","#873eb1","#8a3eb2","#8d3eb2","#903db2","#943db3","#973db3","#9a3db3","#9d3db3","#a13db3","#a43db3","#a73cb3","#aa3cb2","#ae3cb2","#b13cb2","#b43cb1","#b73cb0","#ba3cb0","#be3caf","#c13dae","#c43dad","#c73dac","#ca3dab","#cd3daa","#d03ea9","#d33ea7","#d53ea6","#d83fa4","#db3fa3","#de3fa1","#e040a0","#e3409e","#e5419c","#e8429a","#ea4298","#ed4396","#ef4494","#f14592","#f34590","#f5468e","#f7478c","#f9488a","#fb4987","#fd4a85","#fe4b83","#ff4d80","#ff4e7e","#ff4f7b","#ff5079","#ff5276","#ff5374","#ff5572","#ff566f","#ff586d","#ff596a","#ff5b68","#ff5d65","#ff5e63","#ff6060","#ff625e","#ff645b","#ff6659","#ff6857","#ff6a54","#ff6c52","#ff6e50","#ff704e","#ff724c","#ff744a","#ff7648","#ff7946","#ff7b44","#ff7d42","#ff8040","#ff823e","#ff843d","#ff873b","#ff893a","#ff8c38","#ff8e37","#fe9136","#fd9334","#fb9633","#f99832","#f89b32","#f69d31","#f4a030","#f2a32f","#f0a52f","#eea82f","#ecaa2e","#eaad2e","#e8b02e","#e6b22e","#e4b52e","#e2b72f","#e0ba2f","#debc30","#dbbf30","#d9c131","#d7c432","#d5c633","#d3c934","#d1cb35","#cece36","#ccd038","#cad239","#c8d53b","#c6d73c","#c4d93e","#c2db40","#c0dd42","#bee044","#bce247","#bae449","#b8e64b","#b6e84e","#b5ea51","#b3eb53","#b1ed56","#b0ef59","#adf05a","#aaf159","#a6f159","#a2f258","#9ef258","#9af357","#96f357","#93f457","#8ff457","#8bf457","#87f557","#83f557","#80f558","#7cf658","#78f659","#74f65a","#71f65b","#6df65c","#6af75d","#66f75e","#63f75f","#5ff761","#5cf662","#59f664","#55f665","#52f667","#4ff669","#4cf56a","#49f56c","#46f46e","#43f470","#41f373","#3ef375","#3bf277","#39f279","#37f17c","#34f07e","#32ef80","#30ee83","#2eed85","#2cec88","#2aeb8a","#28ea8d","#27e98f","#25e892","#24e795","#22e597","#21e49a","#20e29d","#1fe19f","#1edfa2","#1ddea4","#1cdca7","#1bdbaa","#1bd9ac","#1ad7af","#1ad5b1","#1ad4b4","#19d2b6","#19d0b8","#19cebb","#19ccbd","#19cabf","#1ac8c1","#1ac6c4","#1ac4c6","#1bc2c8","#1bbfca","#1cbdcc","#1dbbcd","#1db9cf","#1eb6d1","#1fb4d2","#20b2d4","#21afd5","#22add7","#23abd8","#25a8d9","#26a6db","#27a4dc","#29a1dd","#2a9fdd","#2b9cde","#2d9adf","#2e98e0","#3095e0","#3293e1","#3390e1","#358ee1","#378ce1","#3889e1","#3a87e1","#3c84e1","#3d82e1","#3f80e1","#417de0","#437be0","#4479df","#4676df","#4874de","#4a72dd","#4b70dc","#4d6ddb","#4f6bda","#5169d9","#5267d7","#5465d6","#5663d5","#5761d3","#595fd1","#5a5dd0","#5c5bce","#5d59cc","#5f57ca","#6055c8","#6153c6","#6351c4","#6450c2","#654ec0","#664cbe","#674abb","#6849b9","#6a47b7","#6a46b4","#6b44b2","#6c43af","#6d41ad","#6e40aa"]);
+  // const colours=colorNameList.map(color=>color.name);
+  const colours =['#a59344',
+  '#a17c59',
+  '#a9a0a9',
+  '#dfd1bb',
+  '#014b43',
+  '#aaffff',
+  '#3b5861',
+  '#36574e',
+  '#bfc9d0',
+  '#4f4763',
+  '#4d5051',
+  '#aaccee',
+  '#64617b',
+  '#de6a41',
+  '#004683',
+  '#d3b99b',
+  '#f1c766',
+  '#a5a542',
+  '#cfcdbb',
+  '#7ebea5',
+  '#dba3ce',
+  '#8fbfbd',
+  '#e6b4a6',
+  '#353739',
+  '#885555',
+  '#a4896a',
+  '#eb8a44',
+  '#380835',
+  '#505838',
+  '#494c59',
+  '#a47bac',
+  '#c7c7c2',
+  '#aeacac',
+  '#9fb289',
+  '#55aaaa',
+  '#88927e',
+  '#88cc55',
+  '#b67c80',
+  '#b1a3a1',
+  '#97969a',
+  '#ee77ff',
+  '#8a8342',
+  '#eba13c',
+  '#c4a777',
+  '#daeed5',
+  '#e3ecc5',
+  '#b8bfc2',
+  '#eb6123',
+  '#c7f1ff',
+  '#fcc800',
+  '#806257',
+  '#e0e5e2',
+  '#e0cfb5',
+  '#d19431',
+  '#547174',
+  '#ff9a8d',
+  '#b89b72',
+  '#f8ec9e',
+  '#d3e7dc',
+  '#c19fb3',
+  '#cb9e34',
+  '#fca81b',
+  '#e7cfc2',
+  '#ddcc88',
+  '#282e27',
+  '#002266',
+  '#77c593',
+  '#5500ff',
+  '#e8cef6',
+  '#d0bfaa',
+  '#dd8374',
+  '#c2a4c2',
+  '#8a3335',
+  '#ff7913',
+  '#2d3544',
+  '#ffeac5',
+  '#fedcad',
+  '#96ced5',
+  '#6e4826',
+  '#b26ba2',
+  '#aa4c8f',
+  '#ecd9b9',
+  '#e2cdd5',
+  '#6a6283',
+  '#f5e326',
+  '#fdf2c0',
+  '#00968f',
+  '#414833',
+  '#622f2d',
+  '#6c383c',
+  '#ff9bb7',
+  '#dbd8ca',
+  '#72897e',
+  '#077b8a',
+  '#dd835b',
+  '#88ffcc',
+  '#444251',
+  '#f5d180',
+  '#d6edf1',
+  '#82a320',
+  '#7d867c',
+  '#eeefdf',
+  '#c0514a',
+  '#5946b2',
+  '#e0d9cf',
+  '#a76924',
+  '#dcc6a6',
+  '#dcdbca',
+  '#939717',
+  '#f2666c',
+  '#973a36',
+  '#b7b0bf',
+  '#81b89a',
+  '#f6f1e2',
+  '#fc6d84',
+  '#deede4',
+  '#c0a85a']
+  const z =d3.scaleOrdinal(colours);
 
   // total number of nodes
   const n = nodes.length;
@@ -475,16 +597,45 @@ d3.json("10kdata.json", (error, graph) => {
         .on("drag", dragged)
         .on("end", dragended)
     );
-    // var label = svg.selectAll(".mytext")
-		// 				.data(dataset.nodes)
-		// 				.enter()
-		// 				.append("text")
-		// 			    .text(function (d) { return d.name; })
-		// 			    .style("text-anchor", "middle")
-		// 			    .style("fill", "#555")
-		// 			    .style("font-family", "Arial")
-		// 			    .style("font-size", 12);
 
+
+
+    var label = svg.append("g")
+    .attr("class", "labels")
+    .selectAll("text")
+    .data(graph.nodes)
+    .enter().append("text")
+    // .text(function(d) { return d.Category; })
+    .attr("class", "label")
+
+    var tooltip = d3.select("body")
+    .append("div")
+    .style("position", "absolute")
+    .style("visibility", "hidden")
+    .style("color", "white")
+    .style("padding", "8px")
+    .style("background-color", "#626D71")
+    .style("border-radius", "6px")
+    .style("text-align", "center")
+    .style("width", "auto")
+    .text("");
+
+    label 
+    .on("mouseover", function(d){
+                tooltip.html(`${d.Category}`); 
+                return tooltip.style("visibility", "visible");})
+    .on("mousemove", function(){
+        return tooltip.style("top", (d3.event.pageY-10)+"px").style("left",(d3.event.pageX+10)+"px");})
+        circles
+    .on("mouseover", function(d){
+                tooltip.html(`${d.Category}`); 
+                return tooltip.style("visibility", "visible");})
+    .on("mousemove", function(){
+        return tooltip.style("top", (d3.event.pageY-10)+"px").style("left",(d3.event.pageX+10)+"px");})
+    .on("mouseout", function(){return tooltip.style("visibility", "hidden");})
+    ;
+
+    
   const simulation = d3
     .forceSimulation()
     .nodes(nodes)
@@ -510,6 +661,7 @@ d3.json("10kdata.json", (error, graph) => {
       .attr("y2", (d) => d.target.y);
 
     circles.attr("cx", (d) => d.x).attr("cy", (d) => d.y);
+    label.attr("x", (d) => d.x).attr("y", (d) => d.y);
   }
 
   function dragstarted(d) {
